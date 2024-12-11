f247d59484d412bb90ad453c0d4e54d67fc346ce
escolajaimedeoliveira <escolajaimedeoliveira@gmail.com>
2024-12-11 20:02:23 -0300

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -1,32 +1,29 @@
-import styled from "@emotion/styled"
-import Image from "next/image"
-import React from "react"
-import { CONFIG } from "site.config"
-import { Emoji } from "src/components/Emoji"
+import React from 'react';
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import { CONFIG } from 'site.config';
+import { Emoji } from 'src/components/Emoji';
 
-type Props = {}
-
-const ProfileCard: React.FC<Props> = () => {
+const ProfileCard: React.FC = () => {
   return (
     <StyledWrapper>
       <div className="title">
-        <Emoji>💻</Emoji> Profile
+        <Emoji>🏫</Emoji> Escola
       </div>
       <div className="content">
         <div className="top">
-          <Image src={CONFIG.profile.image} fill alt="" />
+          <Image src={CONFIG.profile.image} layout="responsive" width={400} height={300} alt="" />
         </div>
         <div className="mid">
-          <div className=" name">{CONFIG.profile.name}</div>
-          <div className="role">{CONFIG.profile.role}</div>
-          <div className="text-sm mb-2">{CONFIG.profile.bio}</div>
+          <div className="name">EE PEI Jaime de Oliveira</div>
+          <div className="location">📍 Ponte Alta, Barra do Chapéu - SP</div>
         </div>
       </div>
     </StyledWrapper>
-  )
-}
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
 
 const StyledWrapper = styled.div`
   > .title {
@@ -38,7 +35,7 @@ const StyledWrapper = styled.div`
     border-radius: 1rem;
     width: 100%;
     background-color: ${({ theme }) =>
-      theme.scheme === "light" ? "white" : theme.colors.gray4};
+      theme.scheme === 'light' ? 'white' : theme.colors.gray4};
     @media (min-width: 768px) {
       padding: 1rem;
     }
@@ -46,36 +43,24 @@ const StyledWrapper = styled.div`
       padding: 1rem;
     }
     .top {
-      position: relative;
       width: 100%;
-      &:after {
-        content: "";
-        display: block;
-        padding-bottom: 100%;
-      }
+      border-radius: 1rem 1rem 0 0;
     }
     .mid {
       display: flex;
-      padding: 0.5rem;
       flex-direction: column;
       align-items: center;
+      padding: 0.5rem;
       .name {
         font-size: 1.25rem;
         line-height: 1.75rem;
-        font-style: italic;
         font-weight: 700;
       }
-      .role {
-        margin-bottom: 1rem;
+      .location {
         font-size: 0.875rem;
         line-height: 1.25rem;
         color: ${({ theme }) => theme.colors.gray11};
       }
-      .bio {
-        margin-bottom: 0.5rem;
-        font-size: 0.875rem;
-        line-height: 1.25rem;
-      }
     }
   }
-`
+`;
